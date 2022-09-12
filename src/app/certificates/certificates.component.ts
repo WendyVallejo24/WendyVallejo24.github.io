f56bd597e41dbab71f8b33ce2cad4433396800bb
Wendy Vallejo <wbpatraca12@gmail.com>
2022-09-11 23:28:56 -0500

    actualizacion de cambios

--- src/app/certificates/certificates.component.ts
+++ src/app/certificates/certificates.component.ts
@@ -7,31 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CertificatesComponent implements OnInit {
 
-  certificates : Array<any> = []
-
   constructor() { }
 
   ngOnInit(): void {
 
-    let cert1 = {
-      certificacion : "Project Management Professional",
-      anio : "2018"
-    };
-
-    let cert2 = {
-      certificacion : "Ingeniero de Software Certificado",
-      anio : "2020"
-
-    };
-
-    let cert3 = {
-      certificacion : "Certificación de Microsoft",
-      anio : "2022"
-    };
-
-    this.certificates.push(cert1);
-    this.certificates.push(cert2);
-    this.certificates.push(cert3);
   }
-
 }
